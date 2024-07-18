8cc61847707a004017d0a5b12acd232a795dff62
Guillermo Gamarra <162760107+Xakuara@users.noreply.github.com>
2024-07-18 10:30:20 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -101,17 +101,16 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request,dealer_id):
-    if(dealer_id):
-        end
-
-
-
-
-
-[]''sentiment = resresponse''sentiment{}[]rereviewsv:reviewsir
+   if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})eview_detail            print()response"            ''review[]{}()reviewreview_detailanalyze_review_sentimentsanaresporesponse =             il in reviews:_detaviewfor re        ()endpoendpointget_reget_request        reviews = 
-, ""200, ""me:400""status(){} JsoJsonResponseretreturn        dealer_id+strstr()de//dealerviewschRe""fet
+        return JsonResponse({"status":400,"message":"Bad Request"})
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
